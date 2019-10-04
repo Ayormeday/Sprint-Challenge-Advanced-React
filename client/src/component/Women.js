10c432bb97a6af91cb3e1ffdd48ed2f0ad57b916
Ayomide Jones <ayormeday@gmail.com>
2019-10-04 08:21:07 +0100

    dependencies correctly installed

---
+++ client/src/component/Women.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+export default class Women extends Component {
+
+
+    render(){
+        return (
+            <div>
+
+                
+            </div>
+
+
+
+
+
+
+        )
+    }
+}
